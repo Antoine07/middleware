fc4f3cd2378d946b2186ad944f32d6939c73c64c
Antoine07 <antoine.lucsko@gmail.com>
2018-04-18 11:44:25 +0200

    request and response

--- Ennonce_0/Part1/app.php
+++ Ennonce_0/Part1/app.php
@@ -5,3 +5,6 @@ require 'vendor/autoload.php';
 use function Http\Response\send;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+
+$request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
+$response = new \GuzzleHttp\Psr7\Response();
